7ec7d540c1d6e69b323a1ade92aeb31f705846d9
Dave Moz <davejmoz@gmail.com>
2026-03-23 16:25:06 -0400

    Remove config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    MAP_BOX_API_KEY: process.env.MAP_BOX_API_KEY,
-  },
-  async redirects() {
-    return [
-      {
-        source: "/ads.txt",
-        destination: "/public/ads.txt",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
 
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+import("@opennextjs/cloudflare").then((m) => m.initOpenNextCloudflareForDev());
